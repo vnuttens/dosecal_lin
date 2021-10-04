381adf600ad2474a06b6ccfe5e04671f816afe62
vnuttens <84852873+vnuttens@users.noreply.github.com>
2021-10-04 08:55:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+from scipy import stats
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+#--------------------------------------------------------------------------
+
+# IN TE VULLEN
+filename = "26032021.csv"    # file name met data
+time = 36                   # time in minutes between subsequent measurements
+theo_time = 6.0067          # theoretical half life of isotope in hours, Tc99m = 6.0067 h
+
+#--------------------------------------------------------------------------
+
+# Initialisation
+rows = []
+nmeas = []
+date = []
+activity_all = []
+if not os.path.exists('Verwerkte QCs'):
+  os.mkdir('Verwerkte QCs')
+
+
+#--------------------------------------------------------------------------
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile,delimiter = ';')
+
+        # extracting each data row one by one
+        for row in csvreader:
+                rows.append(row)
+
+
+nval = '1'
+rowvalue = 0
+for row in rows:
+ if not row == []:
+   if row[0] == nval:
+        nmeas.append(int(nval)*time/60)
+        for value in row:
+                if len(value) == 15 and value[1] == '/':
+                        date.append(value)
+                if len(value) > 6 and value[-3:] == 'GBq':
+                        nactivity = float(value[:5].replace(',', '.'))*1000
+                        activity_all.append(nactivity)
+                if len(value) > 6 and value[-3:] == 'MBq':
+                        nactivity = float(value[:5].replace(',', '.'))
+                        activity_all.append(nactivity)
+        nval = str(int(nval)+1)
+   rowvalue+=1
+
+# splitting activity in measured and expected
+activity_meas  = []
+activity_theor = []
+
+for i in range(len(activity_all)):
+        if (i % 2 == 0):
+              activity_meas.append(activity_all[i])
+        else:
+              activity_theor.append(activity_all[i])
+
+#--------------------------------------------------------------------------
+# Making plot
+
+fig1, ax1  = plt.subplots(1, 1, sharex=True,sharey=True)
+x1 = np.linspace(0, nmeas[-1], len(nmeas))
+slope, intercept, r_value, p_value, std_err = stats.linregress(nmeas, np.log(activity_meas))
+pract_time = np.log(2)/-slope
+rsqrt = r_value ** 2
+
+ax1.semilogy(nmeas, activity_meas, 'purple', label=f'Gemeten activiteit met t1/2 = {"{:0.3f}".format(pract_time)} h'
+                '\n' r'$\rightarrow$' f' afwijking van {"{:0.2f}".format(np.abs(theo_time-pract_time)/theo_time*100)} % tov {theo_time} h')
+#ax1.semilogy(nmeas, activity_theor, 'b--', label='Theoretical activity')
+ax1.plot(nmeas, np.exp(slope*x1+intercept),color='orange',linestyle='dashed',label=f'Lineare regressie (op semilog) met r² = {"{:0.3f}".format(rsqrt)}')
+
+
+ax1.set_title('QC Dosis Calibrator')
+ax1.set_ylabel('Activiteit [MBq]')
+ax1.set_xlabel('Tijd [h]')
+ax1.legend()
+
+fig1.savefig(os.path.join('Verwerkte QCs',f'{filename}.pdf'))
